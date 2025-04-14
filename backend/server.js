300ac609657e771012fd748db2fe55a600956ad0
pawansuthar01 <paw.kum.2111@gmail.com>
2025-04-14 16:43:24 +0530

    fix: added dotenv to dependencies 3

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
-import dotenv from "dotenv";
-dotenv.config();
+import { config } from "dotenv";
+config();
 import app from "./app.js";
 import cloudinaryPkg from "cloudinary";
 const { v2: cloudinary } = cloudinaryPkg;
