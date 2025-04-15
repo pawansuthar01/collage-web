91d17e6f5995d4f74b419c5ab732a7eda276742c
pawansuthar01 <paw.kum.2111@gmail.com>
2025-04-15 10:21:25 +0530

    backend api update to client side

--- Client/Helper/axiosInstance.ts
+++ Client/Helper/axiosInstance.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosInstance } from "axios";
 
-const BASIC_URL: string = "http://localhost:5000";
+const BASIC_URL: string = "https://collage-web-1.onrender.com";
 
 const axiosInstance: AxiosInstance = axios.create({
   baseURL: BASIC_URL,
