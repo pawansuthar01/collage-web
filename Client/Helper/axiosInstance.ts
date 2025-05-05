c8979d574bf3fa0ef3589c6c3130fcd94821e875
pawansuthar01 <paw.kum.2111@gmail.com>
2025-05-06 00:47:50 +0530

    backend url update...

--- Client/Helper/axiosInstance.ts
+++ Client/Helper/axiosInstance.ts
@@ -1,8 +1,6 @@
 import axios, { AxiosInstance } from "axios";
 
-const BASIC_URL: string = "http://localhost:5001";
-// "https://collage-web-4vba.onrender.com";
-
+const BASIC_URL: string = "https://collage-web-4vba.onrender.com";
 const axiosInstance: AxiosInstance = axios.create({
   baseURL: BASIC_URL,
   withCredentials: true,
